cfc6b09f77625e05530ff85d1facfe26f2082fb4
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2021-06-04 04:27:39 -0600

    ripGame

--- src/Components/CSSDisplay.jsx
+++ src/Components/CSSDisplay.jsx
@@ -1,3 +1,4 @@
+import { element } from 'prop-types';
 import React, { useState } from 'react';
 import VertGame from './VertGame.jsx'
 
@@ -8,19 +9,33 @@ const [GameElements, setElements] = useState([{id:0, elements:[{id: 0, elements:
 
 const reduce =(x, y)=>
 {
+    console.log(x)
+    console.log(y)
     if (x === -1)
     {
-;
+        if (GameElements.length === 1)
+        {
+            return;
+        }
+        setElements(GameElements.filter((el)=>(el.id !== y)));
     }
     else
     {
-        ;
+        const elements = GameElements
+        //elements[y].elements = elements[y].elements[x].elements.pop()
+        console.log(elements[y].elements[x].elements.pop())
+        console.log(elements[y].elements[x].elements.pop())
+        console.log(elements[y].elements[x])
+        const last = elements[y].elements[x].elements.pop()
+        
+        setElements(GameElements.map((el)=>(el.id === y ? {id: el.id, 
+            elements:el.elements[x].elements.map((l)=>(l))} :el)));
     }
 }
 
     return (
         <div className = 'CSSDisp'>
-        {GameElements.map((el)=>(<VertGame horGames = {el.elements} key = {el.id} y = {el.id} />))}
+        {GameElements.map((el)=>(<VertGame horGames = {el.elements} key = {el.id} y = {el.id} reduce = {reduce}/>))}
         </div>
     );
 }
--- src/Components/VertGame.jsx
+++ src/Components/VertGame.jsx
@@ -4,12 +4,11 @@ import HorGame from './HorGame.jsx'
 
 function VertGame({horGames, reduce, augment, y}) {
     const xGames = horGames
-    console.log(xGames)
     return (
         <div className = 'VertGame'>
         <button type='button' onClick = {()=>reduce(-1, y)} className = 'VertGameButton'>-</button>
         <div className = 'VertGameCont'>
-        {xGames.map((x)=>(<HorGame key={x} reduce = {reduce} elements = {x.elements}
+        {xGames.map((x)=>(<HorGame key={x.id+(y+x.id)*10} reduce = {reduce} elements = {x.elements}
         augment = {augment} x = {x.id} y = {y}/>))}
         </div>
         <button type='button' onClick = {()=>augment(-1, y)} className = 'VertGameButton'>+</button>
